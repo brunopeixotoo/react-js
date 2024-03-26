034e408fd930013dbf86e9cefbd1c1daf43fe8e1
Bruno Peixoto <brunopeixotorodrigues546@gmail.com>
2024-03-26 12:12:56 -0400

    react

--- aula-react/src/App.js
+++ aula-react/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState } from 'react';
+
+
+//Componentes
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [nome, setNome] = useState('');//CRIANDO USESTATE PARA CADA INPUT
+  const [email, setEmail] = useState('');
+  const [idade, setIdade] = useState('');
+
+  const [user, setUser] = useState( {} );//CRIANDO UM OBJETO VAZIO USESTATE
+  //Vai ficar responsável por capiturar os valores dos userStates.
+
+  function registar(event) {
+    event.preventDefault();//Vai evitar a atualização do programa
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email
+    })
+  }
+
+  return(
+    <div>
+      <form onSubmit={registar}>
+        <label>Nome: </label><br/>
+        <input
+         placeholder='Digite seu nome'
+         value={nome}
+         onChange={ (event) => setNome(event.target.value)}
+         ></input><br/>
+
+        <label>E-mail: </label><br/>
+        <input 
+        placeholder='Digite seu email'
+        value={email}
+        onChange={ (event) => setEmail(event.target.value)}
+        ></input><br/>
+
+        <label>Idade: </label><br/>
+        <input 
+        placeholder='Digite sua idade'
+        value={idade}
+        onChange={ (event) => setIdade(event.target.value)}
+        ></input><br/>
+    
+        <button type="submit">Registrar</button>
+      </form>
+      <br></br>
+
+      <div>
+        <span>Bem vindo: {user.nome} </span><br/>
+        <span>Idade: {user.idade} anos</span><br/>
+        <span>Email: {user.email} </span><br/>
+
+      </div>
     </div>
   );
 }
 
+//Precisa exportar esse componente.
+//Assim vai conseguir ser acessado por outros arquivos.
 export default App;
---
+++ aula-react/src/components/Nome.js
@@ -0,0 +1,9 @@
+
+//Componente
+function Nome( {aluno, idade} ) {
+    return(
+      <span>Bem Vindo: {aluno} - Idade: {idade} anos</span>
+    );
+  }
+
+  export default Nome;
